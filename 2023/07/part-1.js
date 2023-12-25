749a5cd0a27e52777086ae959365d0f59d086ed1
Paul Evans <paul3vanz@gmail.com>
2023-12-25 22:53:53 +0000

    day 7 part 1

--- 2023/07/part-1.js
+++ 2023/07/part-1.js
@@ -2,54 +2,59 @@ var fs = require('fs');
 
 const start = performance.now();
 
-const input = fs.readFileSync(__dirname + '/inputExample.txt').toString().trim().split('\r\n');
+const input = fs.readFileSync(__dirname + '/input.txt').toString().trim().split('\r\n');
 
 const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
 
-const hands = input.map((line) => {
+const cardScoreMap = cards.map((_, index) => String.fromCharCode(index + 65));
+
+let hands = input.map((line) => {
     const [ hand, bid ] = line.split(' ');
 
     let rank = 0;
 
     const cardGroups = hand.split('').sort().join('').match(/(.)\1*/g).sort((a, b) => b.length - a.length);
 
-    console.log(cardGroups);
+    // console.log(cardGroups);
 
     if (cardGroups[0].length === 5) {
-        rank = 7;
-        console.log('Five of a kind');
+        rank = 7; // Five of a kind
     } else if (cardGroups[0].length === 4) {
-        rank = 6;
-        console.log('Four of a kind');
+        rank = 6; // Four of a kind
     } else if (cardGroups[0].length === 3 && cardGroups[1].length === 2) {
-        rank = 5;
-        console.log('Full house');
+        rank = 5; // Full house
     } else if (cardGroups[0].length === 3 && cardGroups[1].length === 1) {
-        rank = 4;
-        console.log('Three of a kind');
+        rank = 4; // Three of a kind
     } else if (cardGroups[0].length === 2 && cardGroups[1].length === 2) {
-        rank = 3;
-        console.log('Two pair');
+        rank = 3; // Two pair
     } else if (cardGroups[0].length === 2 && cardGroups[1].length === 1) {
-        rank = 2;
-        console.log('One pair');
+        rank = 2; // One pair
     } else {
-        rank = 1;
-        console.log('High card');
+        rank = 1; // High card
     }
+
+    const cardScores = hand.split('').map((card) => cardScoreMap[cards.findIndex((cardScores) => card === cardScores)]).reduce((previous, current) => `${previous}${current}`);
 
     return {
         hand,
         bid,
         rank,
+        cardScores,
     };
 }).sort((a, b) => {
-    b.rank - a.rank
+    return b.rank !== a.rank ? b.rank - a.rank : a.cardScores.localeCompare(b.cardScores);
 });
+
+const winnings = hands.reverse().reduce((previous, current, index) => {
+    // console.log('hand', (index + 1) * parseInt(current.bid));
+    // console.log(previous, current);
+    return previous + ((index + 1) * parseInt(current.bid));
+}, 0)
 
 const end = performance.now();
 
-console.log(hands);
+// console.log(hands);
+// console.log(winnings);
 
-console.log(`Finished in ${(end - start).toFixed(2)}ms`);
+// console.log(`Finished in ${(end - start).toFixed(2)}ms`);
 debugger;
